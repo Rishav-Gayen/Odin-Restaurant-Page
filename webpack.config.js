da706ba9cc60a5be613b0828fd470b150043eb49
Rishav Gayen <gayenrishav19@gmail.com>
2024-12-22 11:34:30 +0000

    Added Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "production",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist"),
+    }
+}
